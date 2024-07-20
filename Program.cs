4c0665a6a0e97a0808ca14f31b7593b6793110e9
Fletcher Moore <fletcherjmoore14@gmail.com>
2024-07-20 12:28:06 -0500

    Commiting changes to phase 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Welcome to Guessing Game!");
+Console.WriteLine("Guess a number (1 - 100)");
+string secretNumberGuess = Console.ReadLine();
+if (int.TryParse(secretNumberGuess, out int secretNumber) && secretNumber > 0 && secretNumber <= 100)
+{
+    Console.WriteLine($"You Guessed {secretNumber}");
+}
+else
+{
+    Console.WriteLine("Please guess a number between 1 and 100. Invalid Input");
+}
