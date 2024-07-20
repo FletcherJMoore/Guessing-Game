7ef7b807e598135e5b4d9b036ba1342f0da87a65
Fletcher Moore <fletcherjmoore14@gmail.com>
2024-07-20 12:41:41 -0500

    Commiting changes requested in Phase 2 of Guessing Game assignment

--- Program.cs
+++ Program.cs
@@ -1,9 +1,19 @@
 ﻿Console.WriteLine("Welcome to Guessing Game!");
 Console.WriteLine("Guess a number (1 - 100)");
-string secretNumberGuess = Console.ReadLine();
-if (int.TryParse(secretNumberGuess, out int secretNumber) && secretNumber > 0 && secretNumber <= 100)
+string userGuessInput = Console.ReadLine();
+
+int secretNumber = 42;
+if (int.TryParse(userGuessInput, out int guessInput) && guessInput > 0 && guessInput <= 100)
 {
-    Console.WriteLine($"You Guessed {secretNumber}");
+    Console.WriteLine($"Checking answer...");
+    if (guessInput == 42)
+    {
+        Console.WriteLine("Congrats you guessed correctly!");
+    }
+    else
+    {
+        Console.WriteLine("That answer is incorrect");
+    }
 }
 else
 {
